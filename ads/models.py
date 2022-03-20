da2b227577c01a070023249f1631e56b5d91cdf4
Krotiks <kate.krot@tut.by>
2022-03-20 23:21:55 +0300

    Iteration 1. Add Ad Model. Add Django Admin

---
+++ ads/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+class Ad(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+    text = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='user_ads')
+    publish = models.DateTimeField(default=timezone.now)
+
+    AD_TYPES = [
+        ('property', 'Недвижимость'),
+        ('auto', 'Авто'),
+        ('tech', 'Электроника'),
+        ('clothes', 'Одежда'),
+        ('kids', 'Детские товары'),
+        ('entertainment', 'Развлечения'),
+        ('services', 'Услуги'),
+    ]
+    ad_types = models.CharField(max_length=25,
+                                     choices=AD_TYPES,
+                                     default='property')
+
+    def __str__(self):
+        return self.title
