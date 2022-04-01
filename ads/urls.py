c72c15b17e34cc9ebd95267d5f6c4f7e175ba398
Krotiks <kate.krot@tut.by>
2022-04-01 12:00:47 +0300

    Iteration 11. Add password reset

--- ads/urls.py
+++ ads/urls.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'ads'
+
+
+class MyHack(auth_views.PasswordResetView):
+    success_url = reverse_lazy("ads:password_reset_done")
+
+
 urlpatterns = [
     #path('', views.all_ads, name='all_ads')
     path('', views.AdListView.as_view(), name='all_ads'),
@@ -15,4 +22,22 @@ urlpatterns = [
     #path('login/', views.custom_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path("password_reset/", MyHack.as_view(), name="password_reset"),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            success_url=reverse_lazy("ads:password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
